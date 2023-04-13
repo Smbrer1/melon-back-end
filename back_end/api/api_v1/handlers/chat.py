7df00bfac7de24285b006ad3d1a37ddbfe65cc07
Соколов Артём Викторович <smbrer1@gmail.com>
2023-04-13 09:24:10 +0300

    quick fix

--- back_end/api/api_v1/handlers/chat.py
+++ back_end/api/api_v1/handlers/chat.py
@@ -113,3 +113,21 @@ async def remove_me_from_chat(chat_id: UUID, user=Depends(get_current_user)):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Message does not exist"
         )
+
+
+@chat_router.get("/me", summary="Find all user chats")
+async def get_my_chats(user: Depends(get_current_user)):
+    """Пост для удаления текущего юзера из чата
+
+    Args:
+        user: DI юзера для jwt токена
+
+    Returns: Универсальная схема удаления
+
+    """
+    try:
+        return await ChatService.get_chats_by_user_id(user.user_id)
+    except pymongo.errors.OperationFailure:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="User does not exist"
+        )
--- back_end/models/user_model.py
+++ back_end/models/user_model.py
@@ -1,19 +1,49 @@
-from datetime import datetime
-from typing import Optional
+from typing import Optional, Union
 from uuid import UUID, uuid4
 
 from beanie import Document, Indexed
-from pydantic import Field, EmailStr
+from phonenumbers import (
+    NumberParseException,
+    PhoneNumberFormat,
+    PhoneNumberType,
+    format_number,
+    is_valid_number,
+    number_type,
+    parse as parse_phone_number,
+)
+from pydantic import Field, EmailStr, constr, validator
+
+MOBILE_NUMBER_TYPES = PhoneNumberType.MOBILE, PhoneNumberType.FIXED_LINE_OR_MOBILE
 
 
 class User(Document):
     user_id: UUID = Field(default_factory=uuid4, alias="userId")
     username: Indexed(str, unique=True)
+    phone_number: constr(max_length=50, strip_whitespace=True) = None
     email: Indexed(EmailStr, unique=True)
     hashed_password: str = Field(alias="hashedPassword")
     first_name: Optional[str] = Field(alias="firstName")
     last_name: Optional[str] = Field(alias="lastName")
     disabled: Optional[bool] = False
+
+    @validator('phone_number')
+    
+    def check_phone_number(self, phone) -> Optional[str]:
+        if phone is None:
+            return phone
+
+        try:
+            parsed_phone = parse_phone_number(phone, 'RU')
+        except NumberParseException as e:
+            raise ValueError('Please provide a valid mobile phone number') from e
+
+        if not is_valid_number(parsed_phone) or number_type(parsed_phone) not in MOBILE_NUMBER_TYPES:
+            raise ValueError('Please provide a valid mobile phone number')
+
+        return format_number(
+            parsed_phone,
+            PhoneNumberFormat.NATIONAL if parsed_phone.country_code == 7 else PhoneNumberFormat.INTERNATIONAL
+        )
 
     def __repr__(self) -> str:
         return f"<User {self.email}>"
